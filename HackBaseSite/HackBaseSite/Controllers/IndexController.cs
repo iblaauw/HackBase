738471cc94932b88da1d00a9d06c3f963ef27afa
eedmond <eedmond@umich.edu>
2013-09-21 03:55:57 -0400

    Initialized VS Site Project

---
+++ HackBaseSite/HackBaseSite/Controllers/IndexController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackBaseSite.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PostHack()
+        {
+            return View();
+        }
+
+    }
+}
