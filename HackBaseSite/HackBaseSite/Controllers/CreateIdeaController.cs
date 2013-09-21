ff29afe9ab5ee2109bd2ddfca196fdedd5ad4eb5
Ian Blaauw <naibla8358@gmail.com>
2013-09-21 05:17:06 -0400

    Moved the filloutform page around and partially hooked it to data

---
+++ HackBaseSite/HackBaseSite/Controllers/CreateIdeaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackBaseSite.Controllers
+{
+    public class CreateIdeaController : Controller
+    {
+        //
+        // GET: /CreateIdea/
+
+        public ActionResult Index()
+        {
+            return View("filloutform");
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return RedirectToAction("Index", "Index");
+        }
+
+    }
+}
