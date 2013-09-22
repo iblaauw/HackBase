3996a1a2f98c9a8081406ec892718d851e80c387
Ian Blaauw <naibla8358@gmail.com>
2013-09-22 00:32:26 -0400

    Made the search bar

---
+++ HackBaseSite/HackBaseSite/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackBaseSite.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        public ActionResult Search(string text)
+        {
+            var database = new MongoClient(IndexController.ConnectionString).GetServer().GetDatabase(IndexController.DatabaseName);
+            var collection = database.GetCollection<Models.HackIdea_Id>("HackIdeas");
+
+            text = text.ToLower();
+
+            var searchResults = collection.FindAll().Where(
+                h => (h.Name ?? "").ToLower().Contains(text) || (h.Author ?? "").ToLower().Contains(text) 
+                    || (h.Description ?? "").ToLower().Contains(text)).ToList();
+
+            return View(searchResults);
+        }
+
+        [HttpPost]
+        public ActionResult SearchTags(string text)
+        {
+            return View();
+        }
+
+    }
+}
